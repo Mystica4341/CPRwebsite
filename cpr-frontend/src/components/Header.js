ceb0c17080b8e6d17c223a31f593cc1557a763eb
tientai <bicute75@gmail.com>
2024-10-04 16:23:49 +0700

    change title of header to menu

--- cpr-frontend/src/components/Header.js
+++ cpr-frontend/src/components/Header.js
@@ -5,14 +5,15 @@ export default function Header() {
   return (
     
     <div className='flex justify-between space-x-3, pl-12'>
-     <img className='w-20 h-20' src='https://i.imgur.com/FyrBIo2.jpeg'></img>
-     
-     <div className='space-x-5 my-auto'>
-      <a className='font-bold text-black hover:text-red-500 transition-colors duration-300-600 mt-5 '>Chicken Frield</a>
-      <a className='font-bold text-black hover:text-red-500 transition-colors duration-300 mt-5 '>Cafe</a>
-      <a className='font-bold text-black hover:text-red-500 transition-colors duration-300 mt-5 ' >Drink</a>
-      <a className='font-bold text-black hover:text-red-500 transition-colors duration-300 mt-5 ' >About Us</a>
       
+     <img className='h-20 w-auto' src='https://i.imgur.com/FyrBIo2.jpeg' ></img>
+     
+     
+     <div className='space-x-5 my-auto font-bold text-black'>
+      <a className=' hover:text-red-500 transition-colors duration-300-600'>Home</a>
+      <a className=' hover:text-red-500 transition-colors duration-300'>Menu</a>
+      <a className=' hover:text-red-500 transition-colors duration-300' >Drink</a>
+      <a className=' hover:text-red-500 transition-colors duration-300' >About Us</a>
       </div>
 
       <div className='flex space-x-2 hover:text-red-500 transition-colors duration-300'>
