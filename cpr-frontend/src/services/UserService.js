8a6749413299bd8f075a65443bd49acfa16a7a1a
Mirera <phudongsi1@gmail.com>
2024-10-30 14:01:41 +0700

    update user service

--- cpr-frontend/src/services/UserService.js
+++ cpr-frontend/src/services/UserService.js
@@ -1,8 +1,21 @@
 import axios from "./custom_axios";
-// import axios from "axios";
 
 const getAllUsers = async () => {
   return await axios.get("api/user");
 };
 
-export { getAllUsers };
+const getUser = async (username) => {
+  return await axios.get(`api/user/${username}`);
+};
+
+//need to change from object to attribute in obj in order to understand
+const addUser = async (user) => {
+  return await axios.post("api/user", user);
+};
+
+//need to change from object to attribute in obj in order to understand
+const updateUser = async (username, user) => {
+  return await axios.put(`api/user/${username}`, user)
+};
+
+export { getAllUsers, getUser, addUser, updateUser };
